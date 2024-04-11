b400774ecd7d4190ba0da7a9d805ef234d37459e
安明诺 <601041341@qq.com>
2024-04-11 09:31:06 +0800

    initial commit

---
+++ src/pages/my/components/avatar.vue
@@ -0,0 +1,51 @@
+<template>
+  <wd-img-cropper v-model="show" :img-src="src" @confirm="handleConfirm" @cancel="handleCancel" />
+  <view class="profile">
+    <view v-if="!imgSrc" class="img" @click="upload">
+      <wd-icon name="fill-camera" custom-class="img-icon"></wd-icon>
+    </view>
+    <wd-img
+      v-if="imgSrc"
+      round
+      width="200px"
+      height="200px"
+      :src="imgSrc"
+      mode="aspectFit"
+      custom-class="profile-img"
+      @click="upload"
+    />
+    <view style="font-size: 14px">点击上传头像</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const src = ref<string>('')
+const imgSrc = ref<string>('')
+const show = ref<boolean>(false)
+
+function upload() {
+  uni.chooseImage({
+    count: 1,
+    success: (res) => {
+      const tempFilePaths = res.tempFilePaths[0]
+      src.value = tempFilePaths
+      show.value = true
+    },
+  })
+}
+function handleConfirm(event) {
+  const { tempFilePath } = event
+  imgSrc.value = tempFilePath
+}
+function imgLoaderror(res) {
+  console.log('加载失败', res)
+}
+function imgLoaded(res) {
+  console.log('加载成功', res)
+}
+function handleCancel(event) {
+  console.log('取消', event)
+}
+</script>
+
+<style lang="scss" scoped></style>
