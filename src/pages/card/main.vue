7b5eef1bd8d0ac55be283131cf0005348602fa72
w ell <3096250047@qq.com>
2024-04-15 22:11:28 +0800

    feat:优化卡片页面

--- src/pages/card/main.vue
+++ src/pages/card/main.vue
@@ -7,23 +7,94 @@
 </route>
 
 <template>
-  <view
-    class="bg-white overflow-hidden pt-2 px-4"
-    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
-  >
-    这里是单词页面哦
-    <!-- <avatar></avatar> -->
+  <view class="bg-white overflow-hidden pt-2 px-4 main-box">
+    <view class="card-box">
+      <view class="top">
+        <view class="text">
+          {{ char.name }}
+          <!-- {{ char.name }} -->
+        </view>
+      </view>
+      <view class="bottom">
+        <view class="box true" @click="goTo('card')">认识</view>
+        <view class="box" @click="goTo('card')">不认识</view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script lang="ts" setup>
+import { onLoad } from '@dcloudio/uni-app'
 import PLATFORM from '@/utils/platform'
 // import avatar from './components/avatar.vue'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
+const char = ref({
+  name: 'Hello World',
+  type: 'text',
+})
+const goTo = (str) => {
+  uni.navigateTo({
+    url: `/pages/${str}/main`,
+  })
+}
+onLoad((options) => {
+  char.value.name += '1'
+})
 </script>
 <style lang="scss" scoped>
-.desc {
-  height: 80rpx;
+.main-box {
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+}
+
+.card-box {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70rpx;
+
+  .top {
+    width: 400rpx;
+    height: 400rpx;
+    line-height: 400rpx;
+    text-align: center;
+
+    .text {
+      font-size: 50rpx;
+      font-weight: bold;
+      color: #333;
+
+      ::after {
+        color: #333;
+        content: '1111111';
+      }
+    }
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 500rpx;
+    height: 400rpx;
+    margin-top: 40rpx;
+
+    .box {
+      width: 150rpx;
+      height: 150rpx;
+      line-height: 150rpx;
+      color: #333;
+      text-align: center;
+      border: 1px solid #333;
+      border-radius: 20rpx;
+    }
+
+    .true {
+      color: #cdbd2f;
+    }
+  }
 }
 </style>
